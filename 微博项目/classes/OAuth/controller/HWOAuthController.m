857f20e18dd0a8b689fe6168035939f7bf8665e4
Youngfev <804434119@qq.com>
2015-12-07 15:44:32 +0800

    OAuth授权

--- 微博项目/classes/OAuth/controller/HWOAuthController.m
+++ 微博项目/classes/OAuth/controller/HWOAuthController.m
@@ -11,11 +11,18 @@
  client_id=3199272345 (AppKey)
  App Secret：ff520801c25170e4078b6f2902eb7474
  redirect_uri=https://api.weibo.com/oauth2/default.html
+ 
+ "access_token" = "2.00oKu2tBrsoVUDeda8a22cceqzG3KD";
+ 
+ 18305141261
+ Yangbin789456
+ 
  */
 
 #import "HWOAuthController.h"
+#import "AFNetworking.h"
 
-@interface HWOAuthController ()
+@interface HWOAuthController ()<UIWebViewDelegate>
 
 @end
 
@@ -27,11 +34,12 @@
     UIWebView *webView = [[UIWebView alloc] init];
     webView.frame = self.view.bounds;
     
+    webView.delegate = self;
     [self.view addSubview:webView];
     
     NSURL *url = [NSURL URLWithString:@"https://api.weibo.com/oauth2/authorize?client_id=3199272345&redirect_uri=https://api.weibo.com/oauth2/default.html"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [webView loadRequest:request];
+    [webView loadRequest:request];//webView
     
 }
 
@@ -40,14 +48,71 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)webViewDidStartLoad:(UIWebView *)webView
+{
+    HWLog(@"webViewDidStartLoad");
 }
-*/
+-(void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    HWLog(@"webViewDidFinishLoad");
+}
+
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    
+    NSString *url = request.URL.absoluteString;
+    NSRange range = [url rangeOfString:@"code="];
+    if (range.length) {
+        NSInteger fromIndex = range.location + range.length;
+        NSString *code = [url substringFromIndex:fromIndex];
+        
+        /**
+         换取一个access Token
+         
+         必选	类型及范围	说明
+         client_id	true	string	申请应用时分配的AppKey。
+         client_secret	true	string	申请应用时分配的AppSecret。
+         grant_type	true	string	请求的类型，填写authorization_code
+         
+         grant_type为authorization_code时
+         必选	类型及范围	说明
+         code	true	string	调用authorize获得的code值。
+         redirect_uri	true	string	回调地址，需需与注册应用里的回调地址一致。
+         
+         
+         https://api.weibo.com/oauth2/access_token
+         */
+        [self accessTokenWithCode:code];
+    }
+    return YES;
+}
+
+
+-(void)accessTokenWithCode:(NSString *)code
+{
+    //请求管理者
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//    AFJSONResponseSerializer
+    //拼接请求参数
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    parameters[@"client_id"] = @"3199272345";
+    parameters[@"client_secret"] = @"ff520801c25170e4078b6f2902eb7474";
+    parameters[@"grant_type"] = @"authorization_code";
+    parameters[@"code"] = code;
+    parameters[@"redirect_uri"] = @"https://api.weibo.com/oauth2/default.html";
+    //发送请求
+    [manager POST:@"https://api.weibo.com/oauth2/access_token" parameters:parameters success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+        //沙盒路径
+        NSString *doc = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+        NSString *path = [doc stringByAppendingPathComponent:@"account.plist"];
+        //将数据写入沙盒
+        [responseObject writeToFile:path atomically:YES];
+        
+        HWLog(@"%@",responseObject);
+    } failure:^(AFHTTPRequestOperation * _Nullable operation, NSError * _Nonnull error) {
+        HWLog(@"%@",error);
+    }];
+}
 
 @end
