8ed47e05bae4c6069f64cf614d4def4ff27b217f
Youngfev <804434119@qq.com>
2015-12-17 20:33:05 +0800

    Revert "Revert "Merge branch 'weibo_fix'""
    This reverts commit 1be7e21c093582def737e46f9fcd35d56a61498d.

---
+++ 微博项目/classes/Main/Other/HWfix.h
@@ -0,0 +1,13 @@
+//
+//  HWfix.h
+//  微博项目
+//
+//  Created by Youngfev on 15/12/17.
+//  Copyright © 2015年 Youngfev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HWfix : NSObject
+
+@end
